343b60438b8d33f280dbc191ae95e78842d29bb9
Denys Volodko <babyleejane@gmail.com>
2024-10-05 13:16:48 +0300

    add video and basic swiper

---
+++ src/js/gallery-swiper.js
@@ -0,0 +1,23 @@
+// import Swiper JS
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation, Pagination],
+  direction: 'horizontal',
+  loop: true,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
